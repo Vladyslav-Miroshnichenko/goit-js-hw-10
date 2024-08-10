a47b4f621d2a3f4293826de9536fa3798416cf53
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-08-10 20:23:29 +0200

    task2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,7 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const delay = document.querySelector('[name = delay]');
+const fulfilled = document.querySelector('[value=fulfilled]');
+const rejected = document.querySelector('[value=rejected]');
+const btn = document.querySelector('[type=submit]');
