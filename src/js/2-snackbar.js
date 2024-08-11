26212f1f950b75097f19356a32f76b02f63ddcad
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-08-11 18:27:17 +0200

    task2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,35 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const delay = document.querySelector('[name = delay]');
-const fulfilled = document.querySelector('[value=fulfilled]');
-const rejected = document.querySelector('[value=rejected]');
+const form = document.querySelector('.form');
 const btn = document.querySelector('[type=submit]');
+
+const onSubmit = event => {
+  event.preventDefault();
+  const state = document.querySelector('input[name="state"]:checked').value;
+  const delay = parseInt(document.querySelector('input[name="delay"]').value);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+  promise
+    .then(delay => {
+      iziToast.success({
+        position: 'topCenter',
+        message: `✅Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        position: 'topCenter',
+        message: `❌Rejected promise in ${delay}ms`,
+      });
+    });
+};
+form.addEventListener('submit', onSubmit);
